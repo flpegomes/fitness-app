3fcc951dee62d725efd6408ce2e16cb8f51db101
flpegomes <felipegomesfurtado@gmail.com>
2018-10-30 22:16:51 -0300

    Commit inicial, porém já esta avançado com as aulas do curso

---
+++ components/FitSlider.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, Slider } from 'react-native'
+
+
+export default function FitSlider ({ max, unit, step, value, onChange }) {
+    return (
+        <View>
+            <Slider 
+                step={step}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
+        </View>
+    )
+}
